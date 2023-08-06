c600b9a59916ad214ef2ac4b351e997268ec07cb
Mistakennnn <stevenrpg380@gmail.com>
2023-08-06 19:32:45 -0400

    Changed the NumberPickers for EditTexts, and changed from Int to Long.

--- app/src/main/java/com/Mistaken/NumberGen/FirstActivity.kt
+++ app/src/main/java/com/Mistaken/NumberGen/FirstActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.view.View
-import android.widget.NumberPicker
+import android.widget.EditText
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.random.Random
@@ -15,24 +15,20 @@ class FirstActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
         var texto = findViewById<TextView>(R.id.num)
-        var selector1 = findViewById<NumberPicker>(R.id.selector1)
-        selector1.minValue=0
-        selector1.maxValue=1000000000
-        var selector2 = findViewById<NumberPicker>(R.id.selector2)
-        selector2.minValue=0
-        selector2.maxValue=1000000000
+        var selector1 = findViewById<EditText>(R.id.selector1)
+        var selector2 = findViewById<EditText>(R.id.selector2)
     }
 
     fun numRandom(view: View)
     {
-        var selector1 = findViewById<NumberPicker>(R.id.selector1)
-        var selector2 = findViewById<NumberPicker>(R.id.selector2)
-        val num1 = selector1.value
-        val num2 = selector2.value
+        var selector1 = findViewById<EditText>(R.id.selector1)
+        var selector2 = findViewById<EditText>(R.id.selector2)
+        val num1 = selector1.text.toString().toLong()
+        val num2 = selector2.text.toString().toLong()
 
         val menor = min(num1, num2)
         val mayor = max(num1, num2)
-        val random = Random.nextInt(menor, mayor+1)
+        val random = Random.nextLong(menor, mayor+1)
         var texto = findViewById<TextView>(R.id.num)
         texto.text = "$random"
 
