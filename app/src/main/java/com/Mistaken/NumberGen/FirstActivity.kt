02a83ad1845720e1e9e27bcc10026dc73a2498dd
Mistakennnn <stevenrpg380@gmail.com>
2023-08-07 02:17:21 -0400

    Added character length limit of 18, and varying the generated number's text size depending on its length, moved the button a bit up.

--- app/src/main/java/com/Mistaken/NumberGen/FirstActivity.kt
+++ app/src/main/java/com/Mistaken/NumberGen/FirstActivity.kt
@@ -31,8 +31,37 @@ class FirstActivity : AppCompatActivity() {
         val random = Random.nextLong(menor, mayor+1)
         var texto = findViewById<TextView>(R.id.num)
         texto.text = "$random"
+        tamañoTexto()
 
         var aviso = findViewById<TextView>(R.id.aviso)
         aviso.text = "Su número generado entre $menor y $mayor es:"
+    }
+
+    fun tamañoTexto()
+    {
+        var display = findViewById<TextView>(R.id.num)
+        display.textSize = when (display.text.length)
+        {
+            1 -> 120f
+            2 -> 100f
+            3 -> 70f
+            4 -> 55f
+            5 -> 40f
+            6 -> 35f
+            7 -> 30f
+            8 -> 27f
+            9 -> 25f
+            10 -> 22f
+            11 -> 20f
+            12 -> 18f
+            13 -> 17f
+            14 -> 15f
+            15 -> 14f
+            16 -> 13f
+            17 -> 12f
+            else -> 11.5f
+        }
+
+
     }
 }
